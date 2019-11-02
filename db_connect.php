6a3c992fe915b3b15db27de886d845d91a13485d
ryuki999 <50240567+ryuki999@users.noreply.github.com>
2019-11-02 15:32:20 +0900

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -5,17 +5,8 @@
     
       $dsn = "mysql:dbname=crystal of knowledge;host=localhost";
       $user = "root";
-      $password = "wonder578";
-    /*
-      $dsn = "mysql:dbname=tb210319db;host=localhost";
-      $user = "tb-210319";
-      $password = "mttwXtjr5Y";
-      
-      
-      $dsn = "mysql:dbname=LAA1070277-8d6jco;host=mysql138.phy.lolipop.lan";
-      $user = "LAA1070277";
-      $password = "EWmB5mIU";
-      */
+      $password = "********";
+
       $pdo=new PDO($dsn,$user,$password,array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
 
       return $pdo;
